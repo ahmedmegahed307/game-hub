8faa7786f7f2a473f93f408550edb148de4c398c
ahmedm <ahmed.mucahit@afsgo.com>
2023-05-17 10:39:49 +0300

    added search bar

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,7 +23,8 @@ const useGames = (gameQuery:GameQuery) => {
   {params:{  //as a query string parameter
     genres:gameQuery.genre?.id,
  platforms:gameQuery.platform?.id,
- ordering:gameQuery.sortOrder
+ ordering:gameQuery.sortOrder,
+ search:gameQuery.searchText
   }
   },
  [gameQuery]
